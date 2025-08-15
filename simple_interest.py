6fee06fe3a7f22beecd3459c63889f705fae559a
GunSlinger0715 <133052505+GunSlinger0715@users.noreply.github.com>
2025-08-15 16:53:11 -0500

    Add simple interest calculator

---
+++ simple_interest.py
@@ -0,0 +1,5 @@
+P = float(input('Enter the principal amount: '))
+R = float(input('Enter the rate of interest: '))
+T = float(input('Enter the time in years: '))
+SI = (P * R * T) / 100
+print(f'Simple Interest: {SI}')
